9c91f8808e9be4366b749170a0fdd2be9fa209dc
Anton <45540051+Spani4@users.noreply.github.com>
2019-04-30 23:57:07 +0300

    Add files via upload

---
+++ DollarToRubble/main.js
@@ -0,0 +1,31 @@
+"use strict"
+
+class DollarToRubble {
+  constructor(field, rate) {
+    this._field = field;
+    this._rate = rate;
+
+    this._init(field, rate);
+  }
+
+  _init(field, rate) {
+
+    let str = field.innerHTML;
+    let reg = /\$(\d+(?:\.\d+)?)/i;
+
+    while ( str.match(reg) ) {
+      let newPrice = this._convert( str.match(reg)[1], rate );
+      str = str.replace( reg, newPrice + ' руб.' );
+    }
+
+    field.innerHTML += '\n' + str;
+  }
+
+  _convert(str, rate) {
+    console.log(Math.round(rate * str * 100) / 100);
+    return (Math.round(rate * str * 100) / 100).toFixed(2);
+  }
+
+}
+
+new DollarToRubble( document.querySelector('body'), 63.80 );
